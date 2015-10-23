45c3a66bfa090633837bac5aaca7697fab998db5
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-10-23 17:19:04 +0300

    Add limit parameter to UserController index method.
    	modified:   app/Http/Controllers/UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\User;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends BaseController
@@ -25,28 +26,14 @@ class UserController extends BaseController
      */
     public function index()
     {
+        $filters = Input::only('limit');
+
+        if ($filters['limit'] != null) {
+
+            return User::all()->take($filters['limit']);
+        }
+
         return User::all();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
     }
 
     /**
