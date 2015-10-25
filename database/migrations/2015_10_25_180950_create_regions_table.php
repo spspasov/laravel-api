b55c2b642e54a30b59ac79f36c00412c2686a6c1
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-10-25 18:24:21 +0200

    Add migration for regions and bus_region pivot table.
    	new file:   database/migrations/2015_10_25_180950_create_regions_table.php

---
+++ database/migrations/2015_10_25_180950_create_regions_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('image_url');
+            $table->string('state');
+            $table->double('lat');
+            $table->double('lon');
+            $table->timestamps();
+        });
+
+        Schema::create('bus_region', function (Blueprint $table) {
+
+            $table->integer('bus_id')->unsigned()->index();
+            $table->foreign('bus_id')->references('id')->on('buses')->onDelete('cascade');
+
+            $table->integer('region_id')->unsigned()->index();
+            $table->foreign('region_id')->references('id')->on('regions')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('regions');
+        Schema::drop('bus_region');
+    }
+}
