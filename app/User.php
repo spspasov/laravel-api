b07fd84529929f940484a346203ac24a50073242
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-10-26 17:06:31 +0200

    Remove active property from fillable array

--- app/User.php
+++ app/User.php
@@ -28,7 +28,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password', 'phone_number', 'active'];
+    protected $fillable = ['name', 'email', 'password', 'phone_number'];
 
     /**
      * The attributes excluded from the model's JSON form.
