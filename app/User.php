79d12cbba951a0081cbb1789297013ef1c41ab93
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-10-26 15:26:38 +0200

    Add active property to the fillable array

--- app/User.php
+++ app/User.php
@@ -28,7 +28,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password', 'phone_number'];
+    protected $fillable = ['name', 'email', 'password', 'phone_number', 'active'];
 
     /**
      * The attributes excluded from the model's JSON form.
