59b5f04bf4dbdd03468ad65bd1e81a074dcb0d93
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-11-01 22:14:36 +0200

    Add email view for user receipt after successful payment

---
+++ resources/views/emails/booking_made_user.blade.php
@@ -0,0 +1,5 @@
+Dear {{ $user->name }},
+<br>
+You successfully deposited {{ $deposit }} for your trip to {{ $region->name }} on {{ $request->date }}
+<br>
+Your payment to {{ $bus->name }} was accepted and a booking was made.
