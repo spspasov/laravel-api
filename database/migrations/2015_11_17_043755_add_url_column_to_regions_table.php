c9c9b3b73b9b8f5ddf35625acdda18e4bcf8d2a3
Svetoslav Spasov <spspasov@Svetoslavs-Mac-Pro.local>
2015-11-16 18:44:21 -0800

    Add url column to regions table 	new file:   database/migrations/2015_11_17_043755_add_url_column_to_regions_table.php

---
+++ database/migrations/2015_11_17_043755_add_url_column_to_regions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUrlColumnToRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('regions', function (Blueprint $table) {
+            $table->string('url')->after('image_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('regions', function (Blueprint $table) {
+            $table->dropColumn('url');
+        });
+    }
+}
