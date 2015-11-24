95c920c92e19856e5f89687d7ddfee3e88bc8bbe
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-11-24 12:36:07 +0200

    Add migration for date of the special non-working days of a venue

---
+++ database/migrations/2015_11_24_122733_add_date_column_to_hours_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDateColumnToHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('hours', function (Blueprint $table) {
+            $table->date('date')->nullable()->after('closed');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hours', function (Blueprint $table) {
+            $table->dropColumn('date');
+        });
+    }
+}
