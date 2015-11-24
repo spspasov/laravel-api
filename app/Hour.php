e968ccba71b5dbf6a950a89bd0322f262592827e
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-11-24 13:35:39 +0200

    Add method for converting the provided date and/or time to an array

--- app/Hour.php
+++ app/Hour.php
@@ -64,4 +64,63 @@ class Hour extends Model
     {
         return $this->belongsTo('App\Venue');
     }
+
+    /**
+     * Return a prettified representation of the data passed
+     *
+     * @param $data
+     * @return array
+     */
+    public static function convertToArray($data)
+    {
+        $hours = $data;
+        $array = [];
+
+        foreach ($hours as $hour) {
+            if ($hour->closed == Hour::CLOSED) {
+                $array[Venue::DAYS_OF_WEEK[$hour->day_of_week]] = "closed";
+            } else if ($hour->closed == Hour::OPEN) {
+                $array[Venue::DAYS_OF_WEEK[$hour->day_of_week]] =
+                self::prettifyTime($hour->open_time) .
+                "-" .
+                self::prettifyTime($hour->close_time);
+            } else if ($hour->closed == Hour::SPECIAL_NON_WORKING_DAY) {
+                $array[$hour->description] = self::prettifyDate($hour->date);
+            } else if ($hour->closed == Hour::OPEN_WITH_REDUCED_WORKING_HOURS) {
+                $array[$hour->description] = [
+                    self::prettifyDate($hour->date) =>
+                        self::prettifyTime($hour->open_time) .
+                        "-" .
+                        self::prettifyTime($hour->close_time)
+                ];
+            }
+        }
+        return $array;
+    }
+
+    /**
+     * Returns a pretty version of the provided date
+     *
+     * The returned date looks like this:
+     *
+     * December 31
+     *
+     * @param $date
+     * @return bool|string
+     */
+    public static function prettifyDate($date)
+    {
+        return date('F d', strtotime($date));
+    }
+
+    /**
+     * Returns a pretty version of the provided time
+     *
+     * @param $time
+     * @return bool|string
+     */
+    public static function prettifyTime($time)
+    {
+        return date('g:ia', strtotime($time));
+    }
 }
