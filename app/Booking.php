1499177efba759cd7d97f82a3d6c383fac997669
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-11-25 18:09:41 +0200

    Add booking model and migration

---
+++ app/Booking.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'bookings';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'venue_id',
+        'request_id',
+        'date',
+        'status',
+        'comments',
+        'pax'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * Return the user that made the booking
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Return the venue this booking was made to
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function venue()
+    {
+        return $this->belongsTo('App\Venue');
+    }
+}
---
+++ database/migrations/2015_11_25_175155_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('venue_id');
+            $table->integer('request_id')->nullable();
+            $table->date('date');
+            $table->integer('status');
+            $table->text('comments')->nullable();
+            $table->string('pax');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bookings');
+    }
+}
