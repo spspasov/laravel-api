38019def3bbca1cc4659b6a9901d63123820f97d
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-11-26 15:39:12 +0200

    Create method stubs

--- app/Http/Controllers/VenuesController.php
+++ app/Http/Controllers/VenuesController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class VenuesController extends Controller
 {
@@ -22,5 +23,38 @@ class VenuesController extends Controller
             return response()->json(['not found' => 'venue not found'], 404);
         }
         return $venue;
+    }
+
+    /**
+     * Validate if the input data matches our requirements
+     *
+     * @param array $data
+     * @return mixed
+     */
+    public function create(Request $request)
+    {
+
+    }
+
+    /**
+     * Validate if the input data matches our requirements
+     *
+     * @param array $data
+     * @return mixed
+     */
+    protected function validator(array $data)
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(array $data)
+    {
+
     }
 }
