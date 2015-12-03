21ae6f259bb862ee5edca05844d7d9d0fac71159
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-12-03 12:21:56 +0200

    Add scheduled task for clearing tokens older than a week every day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Token;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->call(function () {
+            Token::where('created_at', '<', Carbon::today()->subWeek())->delete();
+        })->daily();
     }
 }
