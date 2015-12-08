e1a67b95bb6112f1c81995c5ac0664f65f90b599
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-12-08 11:37:29 +0200

    Change column pax in bookings to an integer

---
+++ database/migrations/2015_12_08_113323_change_pax_column_in_bookings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangePaxColumnInBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->integer('pax')->unsigned()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->text('pax')->change();
+        });
+    }
+}
