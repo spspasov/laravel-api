ae0adaf95e5a9c2ab84d016ea6d39e2f3a2c14f3
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-12-14 22:22:50 +0200

    add method for creating default work hours for venue

--- app/Hour.php
+++ app/Hour.php
@@ -49,6 +49,18 @@ class Hour extends Model
      */
     const SPECIAL_NON_WORKING_DAY = 3;
 
+    /*
+     * Days of the week
+     */
+    const MONDAY = 0;
+    const TUESDAY = 1;
+    const WEDNESDAY = 2;
+    const THURSDAY = 3;
+    const FRIDAY = 4;
+    const SATURDAY = 5;
+    const SUNDAY = 6;
+
+
     /**
      * The attributes excluded from the model's JSON form.
      *
@@ -82,9 +94,9 @@ class Hour extends Model
                 $array[Venue::DAYS_OF_WEEK[$hour->day_of_week]] = "closed";
             } else if ($hour->closed == Hour::OPEN) {
                 $array[Venue::DAYS_OF_WEEK[$hour->day_of_week]] =
-                self::prettifyTime($hour->open_time) .
-                "-" .
-                self::prettifyTime($hour->close_time);
+                    self::prettifyTime($hour->open_time) .
+                    "-" .
+                    self::prettifyTime($hour->close_time);
             } else if ($hour->closed == Hour::SPECIAL_NON_WORKING_DAY) {
                 $array[$hour->description] = self::prettifyDate($hour->date);
             } else if ($hour->closed == Hour::OPEN_WITH_REDUCED_WORKING_HOURS) {
@@ -144,11 +156,11 @@ class Hour extends Model
             return $date;
         }
 
-        $date   = explode("/", $date);
+        $date = explode("/", $date);
 
-        $day    = (int)$date[0];
-        $month  = (int)$date[1];
-        $year   = (int)$date[2] < 2000 ? 2000 + (int)$date[2] : (int)$date[2];
+        $day = (int)$date[0];
+        $month = (int)$date[1];
+        $year = (int)$date[2] < 2000 ? 2000 + (int)$date[2] : (int)$date[2];
 
         return Carbon::create($year, $month, $day, 00, 00, 00);
     }
@@ -162,11 +174,41 @@ class Hour extends Model
     public static function createDateFilters($dates)
     {
         $filterFrom = $dates[0]['from'] ? $dates[0]['from'] : Carbon::today();
-        $filterTo   = $dates[1]['to'] ? $dates[1]['to'] : Carbon::create(2030, 1, 1, 00, 00, 00);
+        $filterTo = $dates[1]['to'] ? $dates[1]['to'] : Carbon::create(2030, 1, 1, 00, 00, 00);
 
-        $from   = Hour::convertDateToCarbon($filterFrom);
-        $to     = Hour::convertDateToCarbon($filterTo);
+        $from = Hour::convertDateToCarbon($filterFrom);
+        $to = Hour::convertDateToCarbon($filterTo);
 
         return [$from, $to];
+    }
+
+    /**
+     * Create default entries for work hours of the specified venue.
+     *
+     * Defaults to:
+     *
+     * Mon - Fri: 09:00 - 17:00
+     * Sat - Sun: Closed
+     *
+     * @param $venueId
+     */
+    public static function createDefaultWorkHours($venueId)
+    {
+        for ($i = 0; $i < 7; $i++) {
+            if ($i < 5) {
+                Hour::create([
+                    'venue_id'    => $venueId,
+                    'day_of_week' => $i,
+                    'open_time'   => '09:00',
+                    'close_time'  => '17:00',
+                ]);
+            } else {
+                Hour::create([
+                    'venue_id'    => $venueId,
+                    'day_of_week' => $i,
+                    'closed'      => Hour::CLOSED,
+                ]);
+            }
+        }
     }
 }
