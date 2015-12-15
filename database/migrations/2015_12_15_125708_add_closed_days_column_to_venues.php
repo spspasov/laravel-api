7e5485aa4175c2919e0c17c987969abb441b1650
Svetoslav Spasov <svetoslav.spasov.89@gmail.com>
2015-12-15 13:11:56 +0200

    add column for the special non working days to the venues table

---
+++ database/migrations/2015_12_15_125708_add_closed_days_column_to_venues.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddClosedDaysColumnToVenues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('venues', function (Blueprint $table) {
+            $table->text('closed_days')->nullable()->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('venues', function (Blueprint $table) {
+            $table->dropColumn('closed_days');
+        });
+    }
+}
